3aa185680d35cd02f5438394bf3beaa014c348b0
Klaustin Wijaya <klaustin.wijaya@gmail.com>
2020-07-02 21:07:52 +0700

    Added more functions in Javascript

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const player = prompt("Hello, please enter your name here: ")
-
+let varScore = 0;
 document.getElementById("pageTitle").innerText = `Welcome, ${player || "you can insert your name by refreshing the page"}!`;
 
-let score = document.getElementById("score").innerText;
 
 function image()
 {
-    score += 1;
+    varScore += 1;
+    newScoreText = varScore;
+    if (varScore == 1)
+    {
+        newScoreText += " Cookie";
+    }
+
+    else
+    {
+        newScoreText += " Cookies";
+    }
+    document.getElementById("scoreText").innerText = newScoreText;
 }
 
-console.log(score)
+function shop()
+{
+    if (varScore >= 50)
+    {
+        console.log("ADD 1 STAR, GET 2 MORE TO WIN THE GAME")
+    }
+
+    else
+    {
+        alert("Insufficient cookie")
+    }
+}
